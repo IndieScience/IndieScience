02ba933c021086465d61849f13bb88e91339593c
Gary Wu <dylanwu@live.com>
2021-04-22 19:30:01 -0500

    add jinja2 template

--- scripts/readme.py
+++ scripts/readme.py
@@ -1,7 +1,12 @@
+from jinja2 import Environment, FileSystemLoader
 import pathlib
 root = pathlib.Path(__file__).parent.parent.resolve()
 
 print(root)
 if __name__ == "__main__":
+    env = Environment(loader=FileSystemLoader('templates'))    
+    template = env.get_template('README.md')
+    text = template.render()
+
     readme = root / "README.md"
-    readme.open("w").write("hello world")
+    readme.open("w").write(text)
